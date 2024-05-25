eae8b273123638f2753ef3b7f8ee57510b45708c
Weslen Almeida <weslengomes@gmail.com>
2024-05-25 15:41:09 -0300

    feat(menu): Criando o menu do app e adcionando a pagian de container

---
+++ lib/pages/container/container_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class ContainerPage extends StatelessWidget {
+
+  const ContainerPage({ super.key });
+
+   @override
+   Widget build(BuildContext context) {
+       return Scaffold(
+           appBar: AppBar(
+            title: const Text('Exemplo de container'),
+          ),
+           body: Center(
+             child: Container(
+              width: 200,
+              height: 200,
+              color: Colors.red,
+              padding: const EdgeInsets.all(40),
+              child: Center(
+                child: Container(
+                  margin: const EdgeInsets.all(1),
+                  decoration: const BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      bottomRight: Radius.circular(30),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black,
+                        blurRadius: 10,
+                        offset: Offset(10, 10)
+                      ),
+                      BoxShadow(
+                        color: Colors.green,
+                        blurRadius: 10,
+                        offset: Offset(-10, 10)
+                      ),
+                    ]
+                  ),
+                ),
+              ),
+             ),
+           ),
+       );
+  }
+}
---
+++ lib/pages/home/home_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+enum PopupMenuPages {
+  container,
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+        backgroundColor: Colors.green,
+        actions: [
+          PopupMenuButton<PopupMenuPages>(
+              tooltip: 'Selecione o menu',
+              onSelected: (PopupMenuPages valueSelected) {
+                switch (valueSelected) {
+                  case PopupMenuPages.container:
+                    Navigator.of(context).pushNamed('/container');
+                    break;
+                }
+              },
+              itemBuilder: (BuildContext context) {
+                return <PopupMenuItem<PopupMenuPages>>[
+                  const PopupMenuItem<PopupMenuPages>(
+                    value: PopupMenuPages.container,
+                    child: Text('Container'),
+                  )
+                ];
+              })
+        ],
+      ),
+      body: Container(),
+    );
+  }
+}
