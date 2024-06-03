ffd01b5472c493130be5e52b12976238bb4025c3
Weslen Almeida <weslengomes@gmail.com>
2024-06-03 06:16:11 -0300

    fix(media query): Media Query por porcentagem

--- lib/pages/media_query/media_query_page.dart
+++ lib/pages/media_query/media_query_page.dart
@@ -5,11 +5,41 @@ class MediaQueryPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.sizeOf(context);
+
+    print('Largura ${mediaQuery.width}');
+    print('Altura ${mediaQuery.height}');
+    print('Orientação ${MediaQuery.maybeOrientationOf(context)}');
+    print('Padding Top ${MediaQuery.paddingOf(context).top}');
+    print('Tamanho AppBarDefault ${kToolbarHeight}');
+    
+    var appBar = AppBar(
+      title: const Text('MediaQuery'),
+    );
+
+    final statusBar = MediaQuery.paddingOf(context).top; //mediaQuery.padding.top;
+    //Tamanho da minha tela
+    final heightBody = mediaQuery.height - statusBar - kToolbarHeight;
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Media Query'),
+      appBar: appBar,
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              color: Colors.red,
+              width: mediaQuery.width * .5,
+              height: heightBody * .5,
+            ),
+            Container(
+              color: Colors.blue,
+              width: mediaQuery.width,
+              height: heightBody * .5,
+            )
+          ],
+          ),
       ),
-      body: Container(),
     );
   }
 }
